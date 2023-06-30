93236f702d30bc615f391ec074c8142e598d33fd
Sreeja_Valasa <0915Sreejav@gmail.com>
2023-06-30 23:20:36 +0530

    commit

---
+++ myweb-app/src/UserCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserCard = ({ user }) => {
+  return (
+    <div className="user-card">
+      <img src={user.avatar} alt={user.name} />
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+    </div>
+  );
+};
+
+export default UserCard;
